23b3e3916c8f0068e08b5bbcda2071eaad6de010
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-10-02 20:30:55 +0200

    testingtests: Character Class Tests with beforeEach() to avoid repeating yourself

--- examples/characters/src/character.test.js
+++ examples/characters/src/character.test.js
@@ -1,22 +1,27 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { Character } from './character.js';
 import { Person } from './person.js';
 
 describe('Character', () => {
 
+  // You can use the beforeEach function to run a function before each test; for repeating setup code
+  let character;
+  beforeEach(() => {
+    character = new Character("Nadja", "Probst", "Player");
+  });
+
+
   it('should create a character with a first name, last name, and role', () => {
-    const character = new Character('Nadja', 'Probst', 'Webdesigner');
+    /* const character = new Character('Nadja', 'Probst', 'Webdesigner'); */
 
     expect(character.firstName).toBe('Nadja');
     expect(character.lastName).toBe('Probst');
-    expect(character.role).toBe('Webdesigner');
-
-
+    expect(character.role).toBe('Player');
     expect(character).toEqual(expect.objectContaining({
       // objectContaining is a partial match; useful when you don't care about all properties
       firstName: 'Nadja',
       lastName: 'Probst',
-      role: 'Webdesigner',
+      role: 'Player',
     }));
   })
 
@@ -33,7 +38,7 @@ describe('Character', () => {
       id: expect.stringContaining('person-'),
       firstName: 'Nadja',
       lastName: 'Probst',
-      role: "Player",
+      role: "Player", 
       level: 1,
       createdAt: expect.any(Date),
       lastModified: expect.any(Date),
@@ -48,7 +53,7 @@ describe('Character', () => {
     });
 
  
-    expect(new Character("Nadja", "Probst", "Player").fullName).toBe('Nadja Probst'); // fullName is inherited from Person
+    expect(character.fullName).toBe('Nadja Probst'); // fullName is inherited from Person
 
     },
   );
@@ -58,9 +63,9 @@ describe('Character', () => {
    /*  expect(() => new Character("Nadja", "Probst", "Player").levelUp()).toBe(2); 
  */
 
-   // 1. Create a new character
-   const character = new Character("Nadja", "Probst", "Player");
-
+   // 1. Create a new character (if not already done in a beforeEach function)
+/*    const character = new Character("Nadja", "Probst", "Player");
+ */
    // 2. Level up the character by calling the levelUp method on the character
    character.levelUp();
 
@@ -71,7 +76,7 @@ describe('Character', () => {
   it('should update the last modified date when leveling up', () => {
 
 
-    const character = new Character("Nadja", "Probst", "Player");
+    /* const character = new Character("Nadja", "Probst", "Player"); */
 
     const createdAt = character.createdAt;
 
@@ -83,8 +88,5 @@ describe('Character', () => {
 
    /*  expect(character.lastModified).toBeGreaterThan(createdAt); */ // we would have to cast the Date object to a number and then compare the numbers
    // https://vitest.dev/api/expect.html#tobegreaterthan
-
-
-
   });
 });
