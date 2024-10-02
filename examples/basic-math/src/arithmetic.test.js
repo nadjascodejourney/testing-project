42e4387713f74cf0bc651737b15f6708491b54ed
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-10-02 17:28:11 +0200

    testingstests: first tests with vitest

--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,6 +1,11 @@
 import { describe, it, expect } from 'vitest';
+import { add } from './arithmetic.js'; // import the function to test it 
 
-describe.todo('add', () => {});
+describe('add', () => {
+    it('should add two positive numbers', () => {
+        expect(add(2,2)).toBe(4);
+    });
+}); // describe means we are describing a test suite, and todo means we are not implementing the test yet
 
 describe.todo('subtract', () => {});
 
