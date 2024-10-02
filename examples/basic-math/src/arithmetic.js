f5c64aa19278740989fa7680520868c8a1dfb13c
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-10-02 18:25:07 +0200

    testingtest: arithmetic tests with type edge cases

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,8 +1,27 @@
 export const add = (a, b) => {
+    if(typeof a === "string" || typeof b === "string") {
+        a = Number(a);
+        b = Number(b);
+    }
+
+    if(isNaN(a)) {
+        throw new Error("Invalid input");
+
+    }
+
+
     return a + b;
+
 };
 
 export const subtract = (a, b) => {
+
+
+
+    if(Array.isArray(a)) {
+        a = a.reduce((a, b) => a - b); // we are using the reduce method to subtract all the numbers in the array from the first number [4, 1] => 4 - 1 = 3
+    }
+
     return a - b;
 };
 
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -5,12 +5,36 @@ describe('add', () => {
     it('should add two positive numbers', () => {
         expect(add(2,2)).toBe(4);
     });
+
+    it('should add two negative numbers', () => {
+        expect(add(-2,-2)).toBe(-4);
+    });
+
+    it("should parse strings into numbers and add them", () => {
+        expect(add('2','2')).toBe(4); // this will fail! => expected 4 but got 22 => we need to fix the add function to parse the string into numbers
+    });
+
+    /* it("should fail, if you give it a string that can't be parsed into a number", () => {
+        expect(add('a', 2)).toBe(2); // this will fail, because this will return NaN
+    }); */
+
+    it("should throw an error, if you give it a string that can't be parsed into a number", () => {
+        expect(() => add('banana', 2)).toThrow("Invalid input"); // we call the add function with a string and a number, and we expect it to throw an error > different syntax than the previous test, because we are testing if the function throws an error
+    });
+
+    
+
+
 }); // describe means we are describing a test suite
 
 describe('subtract', () => {
     it("subtract two positive numbers", () => {
         expect(subtract(3,2)).toBe(1);
     });
+
+    it("should accept and subtract numbers in an array", () => {
+        expect(subtract([4, 1], 2)).toBe(1); // we are passing an array of numbers and a number to subtract; [4, 1] => 4 - 1 = 3 => 3 - 2 = 1
+    }   );
 });
 
 describe('multiply', () => {
