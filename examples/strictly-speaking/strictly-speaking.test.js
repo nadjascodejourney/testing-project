209f563fe6108393379bca2e4af33056b44f0d72
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-10-02 19:42:43 +0200

    testingtests: toBe, toEqual and toStrictEqual

--- examples/strictly-speaking/strictly-speaking.test.js
+++ examples/strictly-speaking/strictly-speaking.test.js
@@ -6,12 +6,17 @@ class Person {
   }
 }
 
+/* test('objects with the same properties are equal', () => {
+  expect({ a: 1, b: 2 }).toBe({ a: 1, b: 2 });
+}); */ // This test will fail, BECAUSE toBe uses strict equality
+
+// .toEqual is a matcher that compares the properties of objects
 test('objects with the same properties are equal', () => {
   expect({ a: 1, b: 2 }).toEqual({ a: 1, b: 2 });
 });
 
 test('objects with different properties are not equal', () => {
-  expect({ a: 1, b: 2 }).not.toEqual({ a: 1, b: 3 });
+  expect({ a: 1, b: 2 }).not.toEqual({ a: 1, b: 3 }); // .not negates the matcher
 });
 
 test('objects with undefined properties are equal to objects without those properties', () => {
@@ -19,7 +24,7 @@ test('objects with undefined properties are equal to objects without those prope
 });
 
 test('objects with undefined properties are *not* strictly equal to objects without those properties', () => {
-  expect({ a: 1 }).not.toStrictEqual({ a: 1, b: undefined });
+  expect({ a: 1 }).not.toStrictEqual({ a: 1, b: undefined }); // .toStrictEqual uses strict equality to compare objects and arrays 
 });
 
 test('instances are equal to object literals with the same properties', () => {
